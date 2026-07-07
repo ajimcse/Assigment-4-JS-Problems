62cbc63464cf4bcab9044cd8624a5e06079cf2dd
ajimcse <mdajimsorkarcse40@gmail.com>
2026-07-07 23:07:50 +0600

    string check

--- assigment-problems.js
+++ assigment-problems.js
@@ -1,10 +1,27 @@
 function calculateTax(income, expenses) {
-    if (income < 0 || expenses < 0 || expenses > income){
+    if (income < 0 || expenses < 0 || expenses > income) {
         return 'Invalid Input'
-    }  
+    }
     const mothleSeving = income - expenses;
     const tex = mothleSeving * 20 / 100;
     return tex;
 }
-const result = calculateTax(100000, 3000)
+// const result = calculateTax(100000, 3000)
+// console.log(result)
+
+
+function sendNotification(email) {
+
+    if (typeof email !== "string" || !email.includes("@")) {
+        return "Invalid email";
+    }
+
+    const parts = email.split("@");
+
+    return parts[0] + " sent you an email from " + parts[1];
+}
+ 
+const result = sendNotification('fahim234.hotmail.com')
 console.log(result)
+
+ 
