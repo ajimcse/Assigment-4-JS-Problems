5d755931b413e6a6679198bc2d626f79c73708a0
ajimcse <mdajimsorkarcse40@gmail.com>
2026-07-08 21:07:51 +0600

    score ber kora

--- assigment-problems.js
+++ assigment-problems.js
@@ -20,23 +20,49 @@ function sendNotification(email) {
 
     return parts[0] + " sent you an email from " + parts[1];
 }
- 
+
 // const result = sendNotification('fahim234.hotmail.com')
 // console.log(result)
 
 function checkDigitsInName(name) {
-         
+
     for (const ch of name) {
         if (ch >= "0" && ch <= "9") {
             return true;
         }
     }
     return false;
- 
+
 }
 
-const result = checkDigitsInName('Raj123')
-console.log(result)
+// const result = checkDigitsInName(' n9ayeem')
+// console.log(result)
+
+
+
+function calculateFinalScore(obj) {
+    if (typeof obj !== "object" || obj === null || Array.isArray(obj)) {
+        return "Invalid Input";
+    }
+    let totalScore = obj.testScore + obj.schoolGrade;
+
+    if (obj.isFFamily === true) {
+        totalScore = totalScore + 20;
+    }
+
+    if (totalScore >= 80) {
+        return true;
+    }
+    return false;
+}
+
+const student = {
+    name: "Rajib",
+    testScore: 45,
+    schoolGrade: 25,
+    isFFamily: true
+};
+
+console.log(calculateFinalScore(student));
 
  
-
