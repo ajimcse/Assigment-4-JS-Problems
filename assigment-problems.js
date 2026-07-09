c1ebfdd5679bf00a77e503580b3aac3049bdff8b
ajimcse <mdajimsorkarcse40@gmail.com>
2026-07-09 18:28:35 +0600

    waitingTime Ber kora

--- assigment-problems.js
+++ assigment-problems.js
@@ -65,4 +65,19 @@ const student = {
 
 console.log(calculateFinalScore(student));
 
- 
+ function waitingTime(waitingTimes, serialNumber) {
+    if (!Array.isArray(waitingTimes) || typeof serialNumber !== 'number') {
+        return 'Invalid Input'
+    }
+    let sum = 0
+    for (const time of waitingTimes) {
+        sum = sum + time
+    }
+    const avg = Math.round(sum / waitingTimes.length);
+    const israt = serialNumber - 1
+    const isratTime = israt - waitingTimes.length
+    const isratSerilTime = isratTime * avg
+    return isratSerilTime
+}
+const resul = waitingTime([ 3, 5, 7, 11, 6 ], 10);
+console.log(resul)
